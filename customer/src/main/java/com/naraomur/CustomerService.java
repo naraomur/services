6ba79fc1d2a42d1a32b0f685bbd8332b7493bf97
Omurbaeva Munara <omtheengi@gmail.com>
2023-04-19 17:57:39 +0600

    Initial commit

---
+++ customer/src/main/java/com/naraomur/CustomerService.java
@@ -0,0 +1,15 @@
+package com.naraomur;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRequest customerRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRequest.firstName())
+                .lastName(customerRequest.lastName())
+                .email(customerRequest.email())
+                .build();
+        customerRepository.save(customer);
+    }
+}
